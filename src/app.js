1cac409c495ad52d3570d058a74cfba364ab8ae1
Tshepiso03 <tshepisosefuba2@gmail.com>
2025-07-03 17:06:43 +0200

    added basic api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
+function refreshWeather(response){
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = response.data.temperature.current;
+
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+
+function searchCity(city) {
+let apiKey = "f65ddcf8t1a9ao05334298873c05002b";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather)
+
+
+}
+
 function handleSearchSubmit(event){
    event.preventDefault();
    let searchInput = document.querySelector("#search-form-input");
    let cityElement = document.querySelector("#city");
    cityElement.innerHTML = searchInput.value ;
-   
+   searchCity(searchInput.value);
+
 }
 
 
